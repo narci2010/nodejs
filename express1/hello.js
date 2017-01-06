63b71fcb4c56974c207334d5ea8cd9d2252edda5
chengzao <c1056660442@163.com>
2017-01-07 01:58:57 +0800

    up

--- express1/hello.js
+++ express1/hello.js
@@ -2,15 +2,15 @@
  * server
  * http://www.gbtags.com/gb/tutorials/222/1173.htm
  */
-// var express = require('express');
-// var app = express();
-// app.get('/', function (req, res) {
-//   res.send('Hello World');
-// })
-// var server = app.listen(8081, function () {
-//   var host = server.address().address
-//   var port = server.address().port
-//   console.log("Example app listening at http://%s:%s", host, port)
-// })
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World');
+})
+var server = app.listen(8081, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Example app listening at http://%s:%s", host, port)
+})
 
 console.log(module.paths);
